947e51a4dcd12c382989d1fad9c8ea52397a621f
ManuelPC20 <48839992+ManuelPC20@users.noreply.github.com>
2019-04-03 11:00:43 -0500

    AunNoFunciona
    :C

---
+++ Proyecto IKEA/Assets/scrips/ArTapToPlaceObject.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+public class ArTapToPlaceObject : MonoBehaviour
+{
+    private ARSessionOrigin arOrigin;
+    private Pose placementPose;
+    private bool placementPoseIsValid = false;
+    [SerializeField]
+    private GameObject placementIndicator;  
+    // Start is called before the first frame update
+    void Start()
+    {
+        arOrigin = FindObjectOfType<ARSessionOrigin>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updatePlaacementPose();
+        upDatePlacemtOmdocator();
+    }
+
+    private void upDatePlacemtOmdocator()
+    {
+        if(placementPoseIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+
+    private void updatePlaacementPose()
+    {
+        var screenCenter = Camera.current.ScreenToViewportPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+        arOrigin.Raycast(screenCenter, hits, UnityEngine.Experimental.XR.TrackableType.Planes);
+
+        placementPoseIsValid = hits.Count > 0;
+        if (placementPoseIsValid)
+        {
+            placementPose = hits[0].pose;
+        }
+
+    }
+}
