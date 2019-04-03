e7579661bb38d18b563314c2e094cd6d3890caf2
ManuelPC20 <48839992+ManuelPC20@users.noreply.github.com>
2019-04-03 17:23:08 -0500

    arCasiFunciona

--- Proyecto IKEA/Assets/scrips/ArTapToPlaceObject.cs
+++ Proyecto IKEA/Assets/scrips/ArTapToPlaceObject.cs
@@ -1,34 +1,44 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.Experimental.XR;
+using System;
 
 public class ArTapToPlaceObject : MonoBehaviour
 {
+    public GameObject objectToPlace;
+    public GameObject placementIndicator;
+
     private ARSessionOrigin arOrigin;
     private Pose placementPose;
     private bool placementPoseIsValid = false;
 
-    [SerializeField]
-    private GameObject placementIndicator;  
-    // Start is called before the first frame update
     void Start()
     {
+     
         arOrigin = FindObjectOfType<ARSessionOrigin>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        UpdatePlaacementPose();
-        UpDatePlacemtImdocator();
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+
+        if (placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            PlaceObject();
+        }
     }
 
-    private void UpDatePlacemtImdocator()
+    private void PlaceObject()
     {
-        if(placementPoseIsValid)
+        Instantiate(objectToPlace, placementPose.position, placementPose.rotation);
+    }
+
+    private void UpdatePlacementIndicator()
+    {
+        if (placementPoseIsValid)
         {
             placementIndicator.SetActive(true);
             placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
@@ -39,24 +49,20 @@ public class ArTapToPlaceObject : MonoBehaviour
         }
     }
 
-    private void UpdatePlaacementPose()
+    private void UpdatePlacementPose()
     {
-        var screenCenter = Camera.current.ScreenToViewportPoint(new Vector3(0.5f,0.5f));
-        Debug.Log(screenCenter);
-
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
         var hits = new List<ARRaycastHit>();
-
-        arOrigin.Raycast(screenCenter, hits,TrackableType.Planes);
+        arOrigin.Raycast(screenCenter, hits, TrackableType.Planes);
 
         placementPoseIsValid = hits.Count > 0;
         if (placementPoseIsValid)
         {
             placementPose = hits[0].pose;
-            var cameraForward = Camera.current.transform.position;
+
+            var cameraForward = Camera.current.transform.forward;
             var cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
             placementPose.rotation = Quaternion.LookRotation(cameraBearing);
-
         }
-
     }
 }
