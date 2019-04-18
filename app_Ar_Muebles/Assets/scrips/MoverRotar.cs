d896c56ccf5aae4f1e3d6f21db1b57836ec20da1
Xtian2108 <christian_2108@live.com>
2019-04-18 15:30:28 -0500

    Botones funcionando con rotación viendo a la camara

--- app_Ar_Muebles/Assets/scrips/MoverRotar.cs
+++ app_Ar_Muebles/Assets/scrips/MoverRotar.cs
@@ -1,27 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class MoverRotar : MonoBehaviour
 {
-
     public bool mover;
     public bool rotar;
     public ObjectController obj;
+
     private void Start()
     {
         obj = GameObject.Find("ObjectController").GetComponent<ObjectController>();
     }
+
+
     public void Mover()
     {
-        mover = true;
-        rotar = false;
+        if(mover == true)
+        {
+            mover = false;
+        }
+        else
+        {
+            mover = true;
+            rotar = false;
+        }
+        
     }
 
     public void Rotar()
     {
-        mover = false;
-        rotar = true;
+        if(rotar == true)
+        {
+            rotar = false;
+        }
+        else
+        {
+            mover = false;
+            rotar = true;
+        }
+        
     }
 
     public void LimpiarBools()
@@ -31,6 +51,7 @@ public class MoverRotar : MonoBehaviour
     }
     public void Borrar()
     {
+        LimpiarBools();
         obj.currentModel.gameObject.SetActive(false);
     }
 }
